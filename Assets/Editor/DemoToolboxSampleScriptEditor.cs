aeeb542d1350509540306574bf55c6c5175c961f
Sasha Krstev <sasha@webfactory.mk>
2020-05-11 10:55:24 +0200

    Added Editor Scripts

---
+++ Assets/Editor/DemoToolboxSampleScriptEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using DemoToolbox;
+
+[CustomEditor(typeof(DemoToolboxSampleScript))]
+public class DemoToolboxSampleScriptEditor : Editor
+{
+    override public void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Test"))
+        {
+            string[] arr = new string[] { "Foo", "Bar" };
+            Debug.Log(arr.RandomElement());
+        }
+    }
+}
---
+++ Assets/Scripts/DemoToolboxSampleScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DemoToolbox;
+
+public class DemoToolboxSampleScript : MonoBehaviour
+{
+    void FooBar()
+    {
+        string[] arr = new string[] {"Foo", "Bar" };
+        arr.RandomElement();
+    }
+}
